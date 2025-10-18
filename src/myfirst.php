7187fee6f51ba094ccda05b2903a501ad2dd8b90
SWH03403 <106224482@student.swin.edu.au>
2025-10-18 08:33:33 +0700

    feat(task-1): simple php script to get array average & status

---
+++ src/myfirst.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Using PHP variables, arrays, and operators</title>
+</head>
+<body>
+	<h1>PHP variables, arrays, and operators</h1>
+<?php
+	$marks = array(85, 85, 95);
+	$marks[1] = 90;
+	$ave = array_sum($marks) / count($marks);
+	$status = $ave >= 50? "PASSED":"FAILED";
+	echo "<p>The average score is $ave. You $status.</p>";
+?>
+</body>
+</html>
