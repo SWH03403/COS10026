07bf269a4184f3f96ef02638aa88d4e34da2c7d5
SWH03403 <106224482@student.swin.edu.au>
2025-10-18 11:03:03 +0700

    feat: simple POST handler that prints all received values

---
+++ part-2/processbooking.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Booking Confirmation</title>
+	<meta charset="utf-8">
+	<meta name="description" content="Rohirrim Booking Form">
+	<meta name="keywords" content="MiddleEarth, Tours, Rohan">
+	<meta name="author" content="Grima Wormtongue">
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link rel="stylesheet" type="text/css" href="style/register.css">
+</head>
+<body>
+
+<header>
+	<h1>Rohirrim Tour Booking Confirmation</h1>
+	<nav>
+		<ul>
+			<li><a href="construction.html">Home</a></li>
+			<li><a href="construction.html">Accommodation</a></li>
+			<li><a href="construction.html">Horse Riding</a></li>
+			<li><a href="construction.html">Sight Seeing</a></li>
+			<li><a href="register.html">Book</a></li>
+		</ul>
+	</nav>
+</header>
+
+<main>
+<?php
+	print_r($_POST);
+?>
+</main>
+
+<footer>
+	<div>
+		<h1 class="fineprint">Conditions Apply</h1>
+		<p class="fineprint">Rohirrim Dude Ranch management takes no responsiblity for any injury, beheadings, spells (sleeping or otherwise), spider-bites suffered by guests, or for anything whatsoever.</p>
+	</div>
+	<p id="contact">Any enquiries please email the <a href="mailto:something@something.com">manager</a></p>
+</footer>
+</body>
+</html>
