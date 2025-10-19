6186f7fea61700fb1fd7ac7caf3eafdf8a228052
SWH03403 <106224482@student.swin.edu.au>
2025-10-20 01:03:59 +0700

    refactor: always set input value & remove `required`

--- src/parts/search_form.php
+++ src/parts/search_form.php
@@ -1,7 +1,5 @@
 <form method="get">
 	<label for="search-model">Search model:</label>
-	<input id="search-model" type="text" name="model"
-		<?php if (!empty($opts['query'])) { echo "value=\"{$opts['query']}\""; } ?>
-		required>
+	<input id="search-model" type="text" name="model" value="<?php echo $opts['query']; ?>">
 	<input type="submit" value=">">
 </form>
