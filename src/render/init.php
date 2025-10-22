0f70d2f4360e8644f1f255aa6e1b807a44b4092f
SWH03403 <106224482@student.swin.edu.au>
2025-10-22 21:28:56 +0700

    refactor: pass `$opts` direct to component render

--- src/render/init.php
+++ src/render/init.php
@@ -1,16 +1,12 @@
 <?php
-	function render(string $part) {
-		global $opts;
-		if (!isset($opts)) { http_response_code(500); exit; }
-		require __DIR__ . "/$part.php";
-	}
+	function render(string $component, array $opts = []) { require __DIR__ . "/$component.php"; }
 
 	function render_page(string|callable $content, array $opts = []) {
-		global $opts; $opts = $opts;
 		$opts['title'] = $opts['title'] ?? 'Unnamed page';
 
-		render('meta/top');
-		if (is_string($content)) { echo "$content"; } else { $content($opts); }
-		render('meta/bottom');
+		render('meta/top', $opts);
+		if (is_string($content)) { echo "$content"; }
+		else { $content(); }
+		render('meta/bottom', $opts);
 	}
 ?>
