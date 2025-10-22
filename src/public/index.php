35d8d5c34f39cbe95978601bf74b43c0467370d3
SWH03403 <106224482@student.swin.edu.au>
2025-10-22 23:56:33 +0700

    feat: render 404 page

--- src/public/index.php
+++ src/public/index.php
@@ -1,11 +1,9 @@
 <?php
 require '../init.php';
 
-function _not_found() { http_response_code(404); exit; }
-
 match (get_uri()) {
 	'/', '/profile' => route('profile'),
 	'/login' => route('login'),
 	'/logout' => route('logout'),
-	default => _not_found(),
+	default => catch_404(),
 };
--- src/routes/init.php
+++ src/routes/init.php
@@ -5,3 +5,5 @@ function is_post(): bool { return get_method() == 'POST'; }
 
 function redirect(string $path) { header("Location: $path"); exit; }
 function route(string $uri) { require __DIR__ . "/$uri.php"; }
+
+function catch_404() { http_response_code(404); render('404'); exit; }
