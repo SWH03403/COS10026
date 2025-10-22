f2dd2ed54d470edb629758dafa3ac11eee4befa8
SWH03403 <106224482@student.swin.edu.au>
2025-10-23 06:44:30 +0700

    feat: database api qol

--- src/core/database.php
+++ src/core/database.php
@@ -1,19 +1,11 @@
 <?php
 class Database {
-	private static bool $has_init = false;
-	private static string $url;
-	private SQLite3 $conn;
-
-	public static function init() {
-		if (self::$has_init) { return; }
-		self::$has_init = true;
-		self::$url = dirname(__DIR__) . "/db.sqlite";
-	}
-
-	public function __construct() { $this->conn = new SQLite3(self::$url); }
+	public function __construct(
+		private SQLite3 $conn = new SQLite3(DATABASE_URL),
+	) {}
 	public function __destruct() { $this->conn->close(); }
 
-	public function query(string $stmt, array $args): array {
+	public function query(string $stmt, array $args = []): array {
 		$query = $this->conn->prepare($stmt);
 		foreach ($args as $idx => $arg) {
 			$type = match (gettype($arg)) {
@@ -21,16 +13,14 @@ class Database {
 				'double' => SQLITE3_FLOAT,
 				'integer' => SQLITE3_INTEGER,
 				'string' => SQLITE3_TEXT,
-				default => exit, // FIX: Be descriptive.
+				default => throw new Exception('Unhandled type: ' . gettype($arg)),
 			};
 			$query->bindValue($idx + 1, $arg, $type);
 		}
 		$result = $query->execute();
-		$result->finalize();
+		if (!str_starts_with($stmt, 'SELECT')) { return []; }
 		$rows = [];
 		while ($row = $result->fetchArray(SQLITE3_ASSOC)) { array_push($rows, $row); }
 		return $rows;
 	}
 }
-
-Database::init();
--- src/init.php
+++ src/init.php
@@ -1,6 +1,10 @@
 <?php
 declare(strict_types = 1);
 
+define('PROJECT_DIR', dirname(__DIR__));
+define('DATABASE_URL', PROJECT_DIR . '/db.sqlite');
+define('MIGRATIONS_DIR', PROJECT_DIR . '/migrations');
+
 require __DIR__ . '/core/init.php';
 require __DIR__ . '/render/init.php';
 require __DIR__ . '/routes/init.php';
