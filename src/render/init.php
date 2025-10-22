6f9db0472770baeb4fe06444de0d858a4920ba4c
SWH03403 <106224482@student.swin.edu.au>
2025-10-22 21:31:11 +0700

    feat: render listed components if input is array

--- src/render/init.php
+++ src/render/init.php
@@ -1,11 +1,12 @@
 <?php
 	function render(string $component, array $opts = []) { require __DIR__ . "/$component.php"; }
 
-	function render_page(string|callable $content, array $opts = []) {
+	function render_page(array|callable|string $content, array $opts = []) {
 		$opts['title'] = $opts['title'] ?? 'Unnamed page';
 
 		render('meta/top', $opts);
 		if (is_string($content)) { echo "$content"; }
+		elseif (is_array($content)) { foreach ($content as $c) { render($c, $opts); }}
 		else { $content(); }
 		render('meta/bottom', $opts);
 	}
