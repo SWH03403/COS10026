1087f9a592b81b60ac69e9ee96f192a821be1504
SWH03403 <106224482@student.swin.edu.au>
2025-10-23 21:31:25 +0700

    feat: show profile data

---
+++ src/render/user_info.php
@@ -0,0 +1,2 @@
+<p>Welcome, <b><?= get_user() ?></b>!</p>
+<p>Email: <b><?= $opts['user']['email'] ?></b></p>
--- src/routes/profile.php
+++ src/routes/profile.php
@@ -1,4 +1,6 @@
 <?php
 $user = get_user();
-if (is_null($user)) { redirect('login'); }
-render_page("<p>Welcome, $user!</p>", ['title' => 'Profile']);
+if (!has_user()) { redirect('login'); }
+$db = new Database();
+$user = $db->query('SELECT email FROM user WHERE name = ?', [get_user()])[0];
+render_page(['user_info'], ['title' => 'Profile', 'user' => $user]);
