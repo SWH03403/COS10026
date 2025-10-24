0c0ad1cd69558376c1b579e9b14e02756c4c3122
SWH03403 <106224482@student.swin.edu.au>
2025-10-24 15:23:37 +0700

    refactor: use hardcoded credentials

--- src/routes/login.php
+++ src/routes/login.php
@@ -9,17 +9,10 @@ if (is_post()) {
 	$csrf = from_form('token');
 
 	if (!check_csrf($csrf)) { array_push($errors, 'Invalid CSRF token'); }
-	if (strlen($user) > 50) { array_push($errors, 'Username is too long'); }
-	if (strlen($pass) > 99) { array_push($errors, 'Password is too long'); } // FIX: Remove if hashed.
-	if (!empty($errors)) { goto end_post; }
-
-	$db = new Database();
-	$rows = $db->query('SELECT password FROM user WHERE name = ?', [$user]);
-	$valid = !empty($rows) && $pass === $rows[0]['password'];
-	if ($valid) { set_user($user); redirect(DEFAULT_ROUTE); }
-	array_push($errors, 'Invalid account credential');
+	if ($user !== 'admin') { array_push($errors, 'Wrong hard-coded username'); }
+	if ($pass !== 'admin') { array_push($errors, 'Wrong hard-coded password'); }
+	if (empty($errors)) { set_user($user); redirect(DEFAULT_ROUTE); }
 }
-end_post:
 
 new_csrf();
 render_page(['login_form', 'errors'], [
