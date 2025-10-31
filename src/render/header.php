8e4db39bf85e35187a135413d8a457ac6258caf6
SWH03403 <106224482@student.swin.edu.au>
2025-10-31 15:58:34 +0700

    feat: profile edit route & form

--- src/render/header.php
+++ src/render/header.php
@@ -1,6 +1,9 @@
 <header>
 	<h1>Generic page header</h1>
 	<nav>
-		<?php if (has_user()) { echo '<a href="/logout">Logout</a>'; } ?>
+		<?php if (has_user()) {
+			echo '<a href="/logout">Logout</a><br>';
+			if (get_uri() != 'edit') { echo '<a href="/edit">Edit profile</a>'; }
+		} ?>
 	</nav>
 </header>
---
+++ src/routes/edit.php
@@ -0,0 +1,12 @@
+<?php
+if (!has_user()) { redirect('login'); }
+
+$errors = [];
+if (is_post()) {
+}
+
+new_csrf();
+render_page(['user_edit', 'errors'], [
+	'title' => 'Edit profile',
+	'errors' => $errors,
+]);
