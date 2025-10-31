7447696f628d34491eced0e20a94ad874a0d7d36
SWH03403 <106224482@student.swin.edu.au>
2025-10-31 16:19:39 +0700

    refactor(render): make `$opts` a global var

--- src/render/init.php
+++ src/render/init.php
@@ -1,12 +1,14 @@
 <?php
-function render(string $component, array $opts = []) { require __DIR__ . "/$component.php"; }
+function render(string $component) { global $opts; require __DIR__ . "/$component.php"; }
 
-function render_page(array|callable|string $content, array $opts = []) {
+function render_page(array|callable|string $content, array $g_opts = []) {
+	global $opts;
+	$opts = $g_opts;
 	$opts['title'] = $opts['title'] ?? 'Unnamed page';
 
-	render('meta/top', $opts);
+	render('meta/top');
 	if (is_string($content)) { echo "$content"; }
-	elseif (is_array($content)) { foreach ($content as $c) { render($c, $opts); }}
+	elseif (is_array($content)) { foreach ($content as $c) { render($c); }}
 	else { $content(); }
-	render('meta/bottom', $opts);
+	render('meta/bottom');
 }
