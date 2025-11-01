443bdbfef6ee5cc40d25a4342b63bba68befa5fe
SWH03403 <106224482@student.swin.edu.au>
2025-11-01 18:19:33 +0700

    feat(render/user_edit): reuse email value from malformed form

--- src/render/user_edit.php
+++ src/render/user_edit.php
@@ -6,7 +6,13 @@
 	<input id="pass-rep" type="password" name="passrep">
 	<br>
 	<label for="email-new">New email</label>
-	<input id="email-new" type="text" name="email" placeholder="(unchanged)">
+	<input
+		id="email-new"
+		type="text"
+		name="email"
+		placeholder="(unchanged)"
+		value="<?= from_form('email') ?>"
+	>
 	<br>
 	<label for="pass-old">Current password</label>
 	<input id="pass-old" type="password" name="pass" required>
