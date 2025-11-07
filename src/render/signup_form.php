2ee79878ec7d119366990068d3f9c0b8699b6712
SWH03403 <106224482@student.swin.edu.au>
2025-11-07 16:48:37 +0700

    feat: implement signup form parse

--- src/render/signup_form.php
+++ src/render/signup_form.php
@@ -2,6 +2,9 @@
 	<label for="username">Username</label>
 	<input id="username" type="text" name="username" value="<?= $opts['username'] ?>" required>
 	<br>
+	<label for="email">Email</label>
+	<input id="email" type="text" name="email" required>
+	<br>
 	<label for="password">Password</label>
 	<input id="password" type="password" name="password" required>
 	<br>
--- src/routes/signup.php
+++ src/routes/signup.php
@@ -3,6 +3,26 @@ if (has_user()) { redirect(DEFAULT_ROUTE); }
 
 $errors = [];
 $user = '';
+if (is_post()) {
+	$user = from_form('username');
+	$email = from_form('email');
+	$pass = from_form('password');
+	$pass2 = from_form('repassword');
+	$csrf = from_form('token');
+
+	if (!check_csrf($csrf)) { $errors[] = 'Invalid CSRF token'; }
+	if (strlen($user) > 50) { $errors[] = 'Username is too long'; }
+	if (strlen($email) > 100) { $errors[] = 'Email is too long'; }
+	if (strlen($pass) > 99) { $errors[] = 'Password is too long'; } // FIX: Remove if hashed.
+	if ($pass != $pass2) { $errors[] = 'Repeat Password does not match'; }
+	if (!empty($errors)) { goto end_post; }
+
+	$db = new Database();
+	$db->query('INSERT INTO user(name, password, email) VALUES (?, ?, ?)', [$user, $pass, $email]);
+	set_user($user);
+	redirect(DEFAULT_ROUTE);
+}
+end_post:
 
 render_page(['signup_form', 'errors'], [
 	'title' => 'Sign Up',
