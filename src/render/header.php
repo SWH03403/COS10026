716e940b570df1a04b9d2c96ed99b2d49ea3b7a5
SWH03403 <106224482@student.swin.edu.au>
2025-11-07 16:40:04 +0700

    feat: global addition nav

--- src/render/header.php
+++ src/render/header.php
@@ -1,9 +1,7 @@
 <header>
 	<h1>Generic page header</h1>
 	<nav>
-		<?php if (has_user()) {
-			echo '<a href="/logout">Logout</a>';
-			echo $opts['nav'];
-		} ?>
+		<?php if (has_user()) { echo '<a href="/logout">Logout</a>'; } ?>
+		<?=	$opts['nav'] ?>
 	</nav>
 </header>
